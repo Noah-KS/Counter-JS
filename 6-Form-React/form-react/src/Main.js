d9394804870fce57b0efa6d3ad60b373a7b54e3f
Noah-KS <nmjohansson7@gmail.com>
2023-12-27 04:43:14 +0900

    Add backend functionality and responsive color functionality for React Form Validation

--- 6-Form-React/form-react/src/Main.js
+++ 6-Form-React/form-react/src/Main.js
@@ -1,23 +1,106 @@
-import React from 'react'
+import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
 
 export default function Main() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+
+  const [errorUsername, setErrorUsername] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const [errorCPassword, setErrorCPassword] = useState("");
+
+  function validate() {
+    if (username.length > 3) {
+      setErrorUsername("");
+    } else {
+      setErrorUsername("Username must be greater than 3 characters long");
+    }
+
+    if (email.length > 8 && email.includes("@")) {
+      setErrorEmail("");
+    } else {
+      setErrorEmail("Must be a valid email");
+    }
+
+    if (password.length > 5) {
+      setErrorPassword("");
+    } else {
+      setErrorPassword("Password must be greater than 5 characters long");
+    }
+
+    if (password === cPassword && password.length > 5) {
+      setErrorCPassword("");
+    } else {
+      setErrorCPassword("Passwords do not match ");
+    }
+  }
+
   return (
     <div>
       <div className="row">
-
         <div className="col-md-4">
           <h1>REACT SIGN UP FORM</h1>
+          <ProgressBar></ProgressBar>
+          <input
+            type="text"
+            placeholder="username"
+            //form-control is first applied, giving orange border.
 
-          <input type = 'text' placeholder = 'username' className = 'form-control' />
-          <input type = 'text' placeholder = 'email' className = 'form-control' />
-          <input type = 'text' placeholder = 'password' className = 'form-control' />
-          <input type = 'text' placeholder = 'confirm password' className = 'form-control' />
+            //if the errorUsername is NOT empty, then form-control-error is applied
+            //if it IS empty, then check username. if username has a value, apply form-control correct.
+            //if it IS empty, and username is also empty, then apply nothing.
 
-          <button className = "btn btn-primary">SUBMIT</button>
+            //the tutorial had a const for every input instead as their approach, but that felt impractical. 
+            //My approach provides separation of concerns, simpler state management, and better performance
+            className={`form-control ${
+              errorUsername ? "form-control-error": username ? "form-control-correct": ""
+            }`}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <p>{errorUsername}</p>
 
+          <input
+            type="text"
+            placeholder="email"
+            className={`form-control ${
+              errorEmail ? "form-control-error": email ? "form-control-correct": ""
+            }`}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <p>{errorEmail}</p>
+
+          <input
+            type="text"
+            placeholder="password"
+            className={`form-control ${
+              errorPassword ? "form-control-error" : password ? "form-control-correct" : ""
+            }`}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p>{errorPassword}</p>
+
+          <input
+            type="text"
+            placeholder="confirm password"
+            className={`form-control ${
+              errorCPassword ? "form-control-error" : cPassword ? "form-control-correct" : ""
+            }`}
+            value={cPassword}
+            onChange={(e) => setCPassword(e.target.value)}
+          />
+          <p>{errorCPassword}</p>
+
+          <button className="btn btn-primary" onClick={validate}>
+            SUBMIT
+          </button>
         </div>
       </div>
-        
     </div>
-  )
+  );
 }
