ebaee0bf20093b9e019aa753089cc902fde0301b
Noah-KS <nmjohansson7@gmail.com>
2023-12-30 23:01:54 +0900

    Implement search functionality

--- 8-Database-React/database-react/src/Main.js
+++ 8-Database-React/database-react/src/Main.js
@@ -1,55 +1,69 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Main() {
+  const [covidData, setCovidData] = useState([]);
+  const [search, setSearch] = useState("");
 
-    const [covidData, setCovidData] = useState([])
+  useEffect(() => {
+    axios
+      .get("https://mahabub81.github.io/covid-19-api/api/v1/countries.json")
+      .then((res) => {
+        console.log(res.data);
+        setCovidData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(()=> {
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
 
-    axios.get('https://mahabub81.github.io/covid-19-api/api/v1/countries.json').then(res=>{
-        console.log(res.data)
-        setCovidData(res.data)
-    }).catch(err=>{console.log(err)})
+  const filteredData = covidData.filter((country) =>
+    country.country_region.toLowerCase().includes(search.toLowerCase())
+  );
 
-}, [])
-
-const tableData = covidData.map((country, index) => (
+  const tableData = filteredData.map((country, index) => (
     <tr key={index}>
-        <td>{country.country_region}</td>
-        <td>{country.population}</td>
-        <td>{country.latest.active}</td>
-        <td>{country.latest.confirmed}</td>
-        <td>{country.latest.deaths}</td>
-        <td>{country.latest.mortality_rate}</td>
+      <td>{country.country_region}</td>
+      <td>{country.population}</td>
+      <td>{country.latest.active}</td>
+      <td>{country.latest.confirmed}</td>
+      <td>{country.latest.deaths}</td>
+      <td>{country.latest.mortality_rate}</td>
     </tr>
-));
+  ));
 
   return (
     <div>
-        <h1>REACT Covid Statistics Database</h1>
+      <h1>REACT Covid Statistics Database</h1>
+      <input
+        type="text"
+        placeholder="Search by Country"
+        value={search}
+        onChange={handleSearchChange}
+      />
 
-        <div className="row">
+      <div className="row">
+        <div className="col-md-8">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Country</th>
+                <th>Population</th>
+                <th>Latest Active</th>
+                <th>Confirmed Cases</th>
+                <th>Deaths</th>
+                <th>Mortality Rate </th>
+              </tr>
+            </thead>
 
-            <div className="col-md-8">
-                <table className = "table">
-                    <thead>
-                        <tr>
-                        <th>Country</th>
-                        <th>Population</th>
-                        <th>Latest Active</th>
-                        <th>Confirmed Cases</th>
-                        <th>Deaths</th>
-                        <th>Mortality Rate </th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {tableData}
-                    </tbody>
-                </table>
-            </div>
+            <tbody>{tableData}</tbody>
+          </table>
         </div>
+      </div>
     </div>
-  )
+  );
 }
